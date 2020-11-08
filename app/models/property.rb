ea003dd79c88a5d3632b7bcf763e5b395ca9c328
Yumiko Kamiyama <tototina.ruru@gmail.com>
2020-11-09 02:33:43 +0900

    add:nearest_station

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,8 @@
 class Property < ApplicationRecord
+  has_many :stations, dependent: :destroy
+  accepts_nested_attributes_for :stations, reject_if: :reject_stations_blank, allow_destroy: true
+
+  def reject_stations_blank(attributes)
+      attributes.merge!(_destroy: "1") if attributes[:routes_name].blank? && attributes[:station_name].blank? && attributes[:minutes_on_foot].blank?
+  end
 end
